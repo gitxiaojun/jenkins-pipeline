b592c2c439f2d8b90c5ebfb6b93eb6c40df85d57
gitxiaojun <39605380@qq.com>
2018-12-29 10:54:31 +0800

    部署脚本
    在客户端主机执行部署操作

---
+++ 部署.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+Path=/opt/test
+TAG=`cat $Path/tag.txt`
+cd $Path
+SS=`grep 20  $Path/docker-compose.yml  |cut -d ":" -f 3`
+sed -i  "s/$SS/$TAG/g"  $Path/docker-compose.yml
+/usr/local/bin/docker-compose down
+
+if /usr/bin/docker images|grep app
+        then /usr/bin/docker images|grep test|awk '{print $1 ":"  $2}'|head -n 1|xargs docker rmi -f
+fi
+
+/usr/local/bin/docker-compose -f /opt/test/docker-compose.yml up -d
